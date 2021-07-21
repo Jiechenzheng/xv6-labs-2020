bde3fd1335299201c0c7c7ac428a1c02d9afe886
jiechenzheng <jiechen.zheng@outlook.com>
2021-07-21 17:01:59 +0800

    save

--- user/sleep.c
+++ user/sleep.c
@@ -3,7 +3,7 @@
 // tick 是由 xv6 kernel 内核定义的时间概念。也可以说是时间片的两次中断之间的时间。
 // 这里并没有给 sleep 做定义
 
-#include "kernel/types.h"
+#include "kernel/types.h"   // types.h 里面有 user.h 要包含的 define
 #include "user/user.h"
 
 
